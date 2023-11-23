afad3bdf9b427b2f132cfadef103a03c80cbb7ba
Preethamraj2003 <96463129+Preethamraj2003@users.noreply.github.com>
2023-11-23 20:43:09 +0530

    Add files via upload

---
+++ Blood Donation Form/app1.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+import datetime
+
+app = Flask(__name__)
+
+# MySQL Configuration
+db_config = {
+    'host': 'localhost',
+    'user': 'sqluser',
+    'password': 'password',
+    'database': 'prg',
+}
+@app.route('/')
+def home():
+    return render_template('blood.html')
+
+@app.route('/data')
+def get_data():
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor(dictionary=True)
+
+    select_data_query = "SELECT * FROM blood_donation2"
+    cursor.execute(select_data_query)
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return render_template('data.html', data=data)
+
+@app.route('/submit',methods=['POST'])
+def Submit_data():
+    if request.method=='POST':
+        name=request.form['name']
+        agegroup=request.form['agegroup']
+        gender=request.form['gender']
+        bloodgroup=request.form['bloodgroup']
+        ph_no=request.form['ph_no']
+        date=datetime.datetime.today().strftime('%Y-%m-%d')
+
+        connection=mysql.connector.connect(**db_config)
+        cursor=connection.cursor()
+
+        Y='INSERT INTO blood_donation2(name,agegroup,gender,bloodgroup,ph_no,date) VALUES (%s,%s,%s,%s,%s,%s)'
+        data=(name,agegroup,gender,bloodgroup,ph_no,date)
+        cursor.execute(Y,data)
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+        
+        return render_template('index.html')
+    else:
+        return redirect(url_for('home'))
+if __name__ == '__main__':
+    app.run(debug=True)
+
